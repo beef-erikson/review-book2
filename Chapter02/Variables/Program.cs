d0e3c713482afe15a9797a17676c2555278acb75
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-01 12:14:06 -0400

    Variables complete.

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -46,6 +46,16 @@ List<Person> people = new()
     new() { FirstName = "Soup"}
 };
 
+Console.WriteLine($"default(int) =  {default(int)}");
+Console.WriteLine($"default(bool) =  {default(bool)}");
+Console.WriteLine($"default(DateTime) =  {default(DateTime)}");
+Console.WriteLine($"default(string) =  {default(string)}");
+
+int number = 13;
+Console.WriteLine($"number has been set to {number}");
+number = default;
+Console.WriteLine($"number has been set to its default: {default}");
+
 class Person
 {
     public DateTime BirthDate;
