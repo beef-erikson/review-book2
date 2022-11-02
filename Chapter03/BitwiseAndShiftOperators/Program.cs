38119f91271c9a904de37a4875b37a0089741206
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-02 14:02:55 -0400

    BooleanOperators / BitwiseAndShiftOperators done

---
+++ Chapter03/BitwiseAndShiftOperators/Program.cs
@@ -0,0 +1,43 @@
+﻿int a = 10; // 00001010
+int b = 6;  // 00000110
+
+WriteLine($"a = {a}");
+WriteLine($"b = {b}");
+WriteLine($"a & b = {a & b}"); // 2-bit column only e.g. 00000010
+WriteLine($"a | b = {a | b}"); // 8, 4, and 2-bit columns e.g. 00001110
+WriteLine($"a ^ b = {a ^ b}"); // 8 and 4-bit columns e.g. 00001100
+
+WriteLine();
+//01010000 Left-shift a by three columns
+WriteLine($"a << 3 = {a << 3}");
+// multiple a by 8
+WriteLine($"a * 8 = {a * 8}");
+// 00000011 right-shift b by one bit column
+WriteLine($"b >> 1 = {b >> 1}");
+
+WriteLine();
+WriteLine("Outputting integers as binary:");
+WriteLine($"a =     {ToBinaryString(a)}");
+WriteLine($"b =     {ToBinaryString(b)}");
+WriteLine($"a & b = {ToBinaryString(a & b)}");
+WriteLine($"a | b = {ToBinaryString(a | b)}");
+WriteLine($"a ^ b = {ToBinaryString(a ^ b)}");
+
+int age = 50;
+
+// How many operators in the following statement?
+char firstDigit = age.ToString()[0];
+// There are four operators:
+// = is the assignment operator
+// . is the member access operator
+// () is the invocation operator
+// [] is the indexer access operator 
+
+
+WriteLine();
+WriteLine($"The {nameof(age)} variable uses {sizeof(int)} bytes of memory.");
+
+static string ToBinaryString(int value)
+{
+    return Convert.ToString(value, toBase: 2).PadLeft(8, '0');
+}
