365e2d9323e18f7bbc1b8f84bad108a14b526ecf
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-04 12:58:51 -0400

    IterationStatements complete.

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,36 @@
+﻿string? password;
+string[] names = { "Soup", "Boot", "Foot" };
+
+int x = 0;
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+
+do
+{
+    if (x >= 0)
+    {
+        Write("Enter your password: ");
+        password = ReadLine();
+        x--;
+    }
+    else
+    {
+        WriteLine("Too many failed attempts!");
+        break;
+    }
+}
+while (password != "Pa$$w0rd");
+WriteLine("Correct!");
