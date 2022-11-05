260440d5ac40b1c95b169c1e05acfe9bf9f3f363
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-05 12:25:55 -0400

    Arrays started

---
+++ Chapter03/Arrays/Program.cs
@@ -0,0 +1,65 @@
+﻿string[] names;
+string[] names2 = new[] { "Beef", "Soup", "Foot", "Boot" }; // array initializer syntax
+
+// allocate memory
+names = new string[4];
+
+// store items
+names[0] = "Kate";
+names[1] = "Jack";
+names[2] = "Rebecca";
+names[3] = "Tom";
+
+// looping through the names
+for (int i = 0; i < names2.Length; i++)
+{
+    WriteLine(names2[i]);
+}
+
+string[,] grid1 = new[,] // two dimensions
+{
+    { "Alpha", "Beta", "Gamma", "Delta" },
+    { "Anne", "Ben", "Charlie", "Doug" },
+    { "Aardvark", "Bear", "Cat", "Dog" }
+};
+
+WriteLine($"Lower bound of the first dimension is: {grid1.GetLowerBound(0)}");
+WriteLine($"Upper bound of the first dimension is: {grid1.GetUpperBound(0)}");
+WriteLine($"Lower bound of the second dimension is: {grid1.GetLowerBound(1)}");
+WriteLine($"Upper bound of the second dimension is: {grid1.GetUpperBound(1)}");
+
+for (int row = 0; row <= grid1.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= grid1.GetUpperBound(1); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {grid1[row, col]}");
+    }
+}
+
+string[][] jagged = new[]
+{
+    new[] { "Alpha", "Beta", "Gamma" },
+    new[] { "Anne", "Ben", "Charlie", "Doug"},
+    new[] { "Aardvark", "Bear" }
+};
+
+WriteLine();
+WriteLine("Upper bound of array of arrays is: {0}", jagged.GetUpperBound(0));
+
+for (int array = 0; array <= jagged.GetUpperBound(0); array++)
+{
+    WriteLine("Upper bound of array {0} is {1}",
+        arg0: array,
+        arg1: jagged[array].GetUpperBound(0));
+}
+
+for (int row = 0; row <= jagged.GetUpperBound(0); row++)
+{
+    for (int col = 0; col <= jagged[row].GetUpperBound(0); col++)
+    {
+        WriteLine($"Row {row}, Column {col}: {jagged[row][col]}");
+    }
+}
+
+int[] sequentialNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] oneTwoNumbers = new int[] { 1, 2 };
