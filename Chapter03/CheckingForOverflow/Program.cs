db0c4cd017c8504d57f26ac262e771f53f55835d
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-07 15:23:15 -0500

    CheckingOverflow complete.

---
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    checked
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("The code overflowed but I caught the exception.");
+}
+
+// this would normally throw a compile error
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+}
