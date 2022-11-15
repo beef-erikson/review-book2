9177746613a700900882fdb2e47cbc84f6ce2c25
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-14 19:34:21 -0500

    Chapter 3 Exercise 5 complete.

---
+++ Chapter03/Ch03Ex05Operators/Program.cs
@@ -0,0 +1,11 @@
+﻿int x = 3;
+int y = 2 + ++x;
+WriteLine($"x is {x} y is {y}");
+
+x = 3 << 2;
+y = 10 >> 1;
+WriteLine($"x is {x} y is {y}");
+
+x = 10 & 8;
+y = 10 | 7;
+WriteLine($"x is {x} y is {y}");
