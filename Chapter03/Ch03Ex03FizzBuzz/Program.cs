a57af287208d078c1facfc54f8f291b0a086331c
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-14 19:00:00 -0500

    Chapter 3 - Exercise 3 complete.

--- Chapter03/Ch03Ex03FizzBuzz/Program.cs
+++ Chapter03/Ch03Ex03FizzBuzz/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <= 100; i++)
+{
+    // fizzbuzz
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Write("Fizzbuzz ");
+        continue;
+    }
+    // fizz
+    else if (i % 3 == 0)
+    {
+        Write("Fizz ");
+        continue;
+    }
+    // buzz
+    else if (i % 5 == 0)
+    {
+        Write("Buzz ");
+        continue;
+    }
+    else
+    {
+        Write($"{i} ");
+    }
+}
