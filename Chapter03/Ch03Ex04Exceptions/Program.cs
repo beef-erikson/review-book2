5d39a1e512489f3ad0bc01cb9cea5c6347e61e44
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-14 19:29:10 -0500

    Chapter 3 Exercise 4 complete.

---
+++ Chapter03/Ch03Ex04Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+    Write("Enter two numbers between 0 and 255. First number: ");
+    int num1 = int.Parse(ReadLine()!);
+    Write("Enter second number: ");
+    int num2 = int.Parse(ReadLine()!);
+
+    WriteLine($"{num1} divided by {num2} is {num1 / num2}.");
+}
+catch (System.FormatException e)
+{
+    WriteLine(e.Message);
+}
+catch (System.Exception e)
+{
+    WriteLine(e);
+    throw;
+}
