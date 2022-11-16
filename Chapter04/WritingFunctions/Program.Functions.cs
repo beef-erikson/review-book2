ef4426f84d57091c1305a20db504ba01bdf84bc7
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-16 00:04:46 -0500

    WritingFunctions complete.

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -15,24 +15,22 @@ partial class Program
         decimal rate = twoLetterRegionCode switch
         {
             "CH" => 0.08M,
-            "DK" => 0.25M,
-            "NO" => 0.25M,
-            "GB" => 0.2M,
-            "FR" => 0.2M,
+            "DK" or "NO" => 0.25M,
+            "GB" or "FR" => 0.2M,
             "HU" => 0.27M,
-            "OR" => 0.0M,
-            "AK" => 0.0M,
-            "MT" => 0.0M,
-            "ND" => 0.05M,
-            "WI" => 0.05M,
-            "ME" => 0.05M,
-            "VA" => 0.05M,
+            "OR" or "AK" or "MT" => 0.0M,
+            "ND" or "WI" or "ME" or "VA" => 0.05M,
             "CA" => 0.0825M,
             _ => 0.06M
         };
         return amount * rate;
     }
 
+    /// <summary>
+    /// Pass a 32-bit integer and it will be converted into its ordinal equivalent.
+    /// </summary>
+    /// <param name="number">Number as a cardinal value e.g. 1, 2, 3, and so on.</param>
+    /// <returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on.</returns>
     static string CardinalToOrdinal(int number)
     {
         int lastTwoDigits = number % 100;
@@ -65,5 +63,89 @@ partial class Program
             Write($"{CardinalToOrdinal(number)} ");
         }
         WriteLine();
+    }
+
+    static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException(
+                message: $"The factorial function is defined for non-negative integers only. Input: {number}",
+                paramName: nameof(number));
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            checked
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+
+    static void RunFactorial()
+    {
+        for (int i = -2; i <= 15; i++)
+        {
+            try
+            {
+                WriteLine($"{i}! = {Factorial(i):N0}");
+            }
+            catch (OverflowException)
+            {
+                WriteLine($"{i}! is too big for a 32-bit integer.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{i}! throws {ex.GetType()}: {ex.Message}");
+            }
+        }
+    }
+
+    static int FibImperative(int term)
+    {
+        if (term == 1)
+        {
+            return 0;
+        }
+        else if (term == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return FibImperative(term - 1) + FibImperative(term - 2);
+        }
+    }
+
+    static void RunFibImperative()
+    {
+        for (int i = 1; i <= 30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+                arg0: CardinalToOrdinal(i),
+                arg1: FibImperative(term: i));
+        }
+    }
+
+    static int FibFunctional(int term) =>
+        term switch
+        {
+            1 => 0,
+            2 => 1,
+            _ => FibFunctional(term - 1) + FibFunctional(term - 2)
+        };
+    
+    static void RunFibFunctional()
+    {
+        for (int i = 1; i <= 30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+                arg0: CardinalToOrdinal(i),
+                arg1: FibFunctional(term: i));
+        }
     }
 }
