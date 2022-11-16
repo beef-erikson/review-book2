02ce10fab1bbb24cdd1d2c923014a80c005385c0
Troy Martin <beef.erikson.studios@gmail.com>
2022-11-15 22:17:57 -0500

    Program.Functions added.

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -9,4 +9,61 @@ partial class Program
         }
         WriteLine();
     }
+
+    static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+    {
+        decimal rate = twoLetterRegionCode switch
+        {
+            "CH" => 0.08M,
+            "DK" => 0.25M,
+            "NO" => 0.25M,
+            "GB" => 0.2M,
+            "FR" => 0.2M,
+            "HU" => 0.27M,
+            "OR" => 0.0M,
+            "AK" => 0.0M,
+            "MT" => 0.0M,
+            "ND" => 0.05M,
+            "WI" => 0.05M,
+            "ME" => 0.05M,
+            "VA" => 0.05M,
+            "CA" => 0.0825M,
+            _ => 0.06M
+        };
+        return amount * rate;
+    }
+
+    static string CardinalToOrdinal(int number)
+    {
+        int lastTwoDigits = number % 100;
+
+        switch (lastTwoDigits)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return $"{number:N0}th";
+            default:
+                int lastDigit = number % 10;
+
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+
+                return $"{number:N0}{suffix}";
+        };
+    }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (int number = 1; number <= 1500; number++)
+        {
+            Write($"{CardinalToOrdinal(number)} ");
+        }
+        WriteLine();
+    }
 }
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,4 @@
-﻿TimesTable(7);
+﻿// TimesTable(22, 20);
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "GB");
+WriteLine($"You must pay {taxToPay} in tax.");
+RunCardinalToOrdinal();
