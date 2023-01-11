b72ff882566b2ef4ebef0df61161294c960df7fe
Troy Martin <beef.erikson.studios@gmail.com>
2023-01-11 17:50:47 -0500

    Exercise started

---
+++ Chapter04/Ch04Ex02PrimeFactorsLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace Ch04Ex02PrimeFactorsLib;
+static class PrimeFactors
+{
+    static string PrimeFactors(int num)
+    {
+        if (num > 1000)
+        {
+            return "Number is too great. Must be 1000 or less.";
+        }
+    }
+}
