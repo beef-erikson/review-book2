42a91eda99dede41289d4d9597d7faea2d64b44c
Troy Martin <beef.erikson.studios@gmail.com>
2023-01-11 18:05:16 -0500

    Exercise pre-testing

--- Chapter04/Ch04Ex02PrimeFactorsLib/Class1.cs
+++ Chapter04/Ch04Ex02PrimeFactorsLib/Class1.cs
@@ -3,9 +3,23 @@ static class PrimeFactors
 {
     static string PrimeFactors(int num)
     {
-        if (num > 1000)
+        string response = "";
+
+        // Invalid number range
+        if (num > 1000 || num < 2)
         {
-            return "Number is too great. Must be 1000 or less.";
+            return "Number is not valid. Range is between 2 and 1000.";
         }
+
+        for (int i = 2; i <= num; i++)
+        {
+            // find out if divisable evenly. If so, add to string
+            if (i % num == 0)
+            {
+                response += $"{i} ";
+            }
+        }
+
+        return response;
     }
 }
